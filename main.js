243793e1eca829d21be8a2c39580d2ce7ae978bc
grassfinn <theharper@live.com>
2022-07-28 21:53:41 -0400

    Testdata setup and grid layout

--- main.js
+++ main.js
@@ -1,48 +1,74 @@
 // https://rapidapi.com/weatherbit/api/weather/
 //https://github.com/browserify/browserify
-
-
-// testData = testData.data.splice(0,10)
-// console.log('test',JSON.stringify(testData));
+// https://www.weatherbit.io/api/weather-forecast-16-day
 const currentDay = document.getElementById('current-day');
-
 const currentWeather = document.getElementById('current-weather');
 const currentTemp = document.getElementById('current-temp');
 const currentWind = document.getElementById('current-wind');
+const axios = require('axios');
 
-async function getData(){
-  const response = await fetch('testData.json')
-  const data = await response.json()
-  return data
+const options = {
+  method: 'GET',
+  url: 'https://weatherbit-v1-mashape.p.rapidapi.com/forecast/daily',
+  params: { lat: '31.15', lon: '-81.36' },
+  headers: {
+    'X-RapidAPI-Key': '51e6d9c0admsh04df8c92ea0b8b7p1714c9jsnffa47057296b',
+    'X-RapidAPI-Host': 'weatherbit-v1-mashape.p.rapidapi.com',
+  },
+};
+
+async function TestData() {
+  const response = await fetch('testData.json');
+  const data = await response.json();
+  console.log(data);
+  return data;
 }
-getData()
 
+TestData().then((response) => {
+  return (document.querySelector('main').innerHTML = displayData(response));
+});
+// // call 1
+// axios
+//   .request(options)
+//   .then(function (response) {
+//     let sixteenDayForecast = response.data.data;
+//     console.log(response.data.data);
+//     document.querySelector('main').innerHTML = displayData(sixteenDayForecast);
+//   })
+//   .catch(function (error) {
+//     console.error(error);
+//   });
+
+// get data html for one day
+function getDataHtml(day) {
+  // map through the array and create the HTML needed
+  return ` <div class="single-day-forecast">
+          <div class="day"><h2>${day.datetime}</h2> </div>
+          <div class="icon"><img src="https://www.weatherbit.io/static/img/icons/${
+            day.weather.icon
+          }.png"></div>
+          <div class="temp">Current Temp ${cToF(day.temp)} High ${cToF(
+    day.high_temp
+  )} Low ${cToF(day.low_temp)}</div>
+          <div class="rain"> Chance of Rain: ${Math.round(day.pop)}%</div>
+          <div class="wind">Wind Speed: ${day.wind_spd} Wind Direction: ${
+    day.wind_cdir_full
+  }</div>
+        </div>
+      `;
+}
+
+// get the data and html for the forecast arr
+function displayData(forecastArr) {
+  return `<div class="sixteen-day-forecast">${forecastArr
+    .map(getDataHtml)
+    .join('')}</div>`;
+}
 
 function cToF(temp) {
   return Math.round(temp * 1.8 + 32);
 }
 const date = new Date();
-
-// const axios = require('axios');
-
-// const options = {
-//   method: 'GET',
-//   url: 'https://weatherbit-v1-mashape.p.rapidapi.com/forecast/daily',
-//   params: { lat: '31.15', lon: '-81.36' },
-//   headers: {
-//     'X-RapidAPI-Key': '51e6d9c0admsh04df8c92ea0b8b7p1714c9jsnffa47057296b',
-//     'X-RapidAPI-Host': 'weatherbit-v1-mashape.p.rapidapi.com',
-//   },
-// };
-
-// axios
-//   .request(options)
-//   .then(function (response) {
-//     console.log(response.data);
-//   })
-//   .catch(function (error) {
-//     console.error(error);
-//   });
 
 //   important info to know:
 // TODO
@@ -55,17 +81,3 @@ const date = new Date();
 
 currentDay.textContent = `${date.toDateString()}`;
 // display data
-function getDataHtml(arr) {
-   let tenDayForecast =  arr.map((day, index) => {
-      // console.log(index,day)
-      document.querySelector('main').innerHTML += ` <div class="day-${index}-forecast">
-        <div class="">${day.datetime}
-        <div class="">Current Temp ${cToF(day.temp)} High ${day.high_temp} Low ${day.low_temp}</div>
-        <div class=""> Chance of Rain: ${Math.round(day.precip * 100)}%</div>
-        <div class="">Wind Speed: ${day.wind_spd} Wind Direction: ${day.wind_cdir_full}</div>
-      </div>`
-    }).join('')
-    return `<div class="ten-day-forecast">${tenDayForecast}</div>`
-      }
-
-displayData(testData)
