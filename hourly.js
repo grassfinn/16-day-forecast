74a413eaba10249b39820954efb36e3a2f457a24
grassfinn <theharper@live.com>
2022-08-09 00:30:35 -0400

    Deployment Test 3

--- hourly.js
+++ hourly.js
@@ -2,15 +2,16 @@
 //https://github.com/browserify/browserify
 // https://www.weatherbit.io/api/weather-forecast-16-day
 // https://day.js.org/en/
+const axios = require('axios');
 
-
-const dayjs = require('dayjs')
-const calendar = require('dayjs/plugin/calendar')
+const dayjs = require('dayjs');
+const calendar = require('dayjs/plugin/calendar');
+const date = new Date();
+const day = date.getDay();
 
 //import dayjs from 'dayjs' // ES 2015
 // const dateToString = dayjs('2022-07-20').toString()
 // console.log(dateToString.substring(0,3))
-const axios = require('axios');
 
 // async function TestData() {
 //   const response = await fetch('testData.json');
@@ -22,21 +23,22 @@ const axios = require('axios');
 // TestData().then((response) => {
 //   return (document.querySelector('main').innerHTML = displayData(response));
 // });
+
 // call 1
 const options = {
-    method: 'GET',
-    url: 'https://weatherbit-v1-mashape.p.rapidapi.com/forecast/hourly',
-    params: { lat: '31.15', lon: '-81.36' },
-    headers: {
-      'X-RapidAPI-Key': '51e6d9c0admsh04df8c92ea0b8b7p1714c9jsnffa47057296b',
-      'X-RapidAPI-Host': 'weatherbit-v1-mashape.p.rapidapi.com'
-    }
-  };
-  
-  axios
+  method: 'GET',
+  url: 'https://weatherbit-v1-mashape.p.rapidapi.com/forecast/hourly',
+  params: { lat: '31.15', lon: '-81.36' },
+  headers: {
+    'X-RapidAPI-Key': '51e6d9c0admsh04df8c92ea0b8b7p1714c9jsnffa47057296b',
+    'X-RapidAPI-Host': 'weatherbit-v1-mashape.p.rapidapi.com',
+  },
+};
+
+axios
   .request(options)
   .then(function (response) {
-    console.log(response)
+    console.log(response);
     let forecast = response.data.data;
     console.log(response.data.data);
     document.querySelector('main').innerHTML = displayData(forecast);
@@ -44,23 +46,24 @@ const options = {
   .catch(function (error) {
     console.error(error);
   });
+
 // get data html for one day
 function getDataHtml(day) {
-  let dateToString = (dayjs(day.datetime)).toString()
-  let currentDate = day.datetime
+  let dateToString = dayjs(day.datetime).toString();
+  let currentDate = day.datetime;
   // map through the array and create the HTML needed
   return ` <div class="single-day-forecast">
-          <div class=""><h3>${currentDate.substring(5)}</h3></div>
-          <div class="icon"><img src="https://www.weatherbit.io/static/img/icons/${
-            day.weather.icon
-          }.png"></div>
-          <div class="temp">Current Temp ${cToF(day.temp)}</div>
-          <div class="rain"> Chance of Rain: ${Math.round(day.pop)}%</div>
-          <div class="wind">Wind Speed: ${day.wind_spd} Wind Direction: ${
+  <div class=""><h3>${currentDate.substring(5)}</h3></div>
+  <div class="icon"><img src="https://www.weatherbit.io/static/img/icons/${
+    day.weather.icon
+  }.png"></div>
+  <div class="temp">Current Temp ${cToF(day.temp)}</div>
+  <div class="rain"> Chance of Rain: ${Math.round(day.pop)}%</div>
+  <div class="wind">Wind Speed: ${day.wind_spd} Wind Direction: ${
     day.wind_cdir_full
   }</div>
-        </div>
-      `;
+  </div>
+  `;
 }
 
 // get the data and html for the forecast arr
@@ -73,7 +76,6 @@ function displayData(forecastArr) {
 function cToF(temp) {
   return Math.round(temp * 1.8 + 32);
 }
-const date = new Date();
 
 //   important info to know:
 // TODO
@@ -83,7 +85,6 @@ const date = new Date();
 // precip
 //  weather icon
 // weather desc
-const day = date.getDay()
 // console.log(day)
 // currentDay.textContent = `${date.toDateString()}`;
 // display data
